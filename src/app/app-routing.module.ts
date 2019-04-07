7ee945e18fc6d8d7bb331734a60fb3734aba6b38
arash bahreini <vbhost.ir@gmail.com>
2019-04-07 17:25:17 +0430

    Design parts of project.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    loadChildren: './login/login.module#LoginModule'
+  },
+  { path: '', component: DashboardComponent },
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true} )],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
