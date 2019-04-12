4270107a7655034915f3db3a30434588bbe6584a
arash bahreini <vbhost.ir@gmail.com>
2019-04-12 19:22:09 +0430

    Fix offset issue in the main page.

--- src/app/home/about-me/about-me.component.ts
+++ src/app/home/about-me/about-me.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -11,6 +11,7 @@ export class AboutMeComponent implements OnInit {
   @ViewChild('bio') public bio: ElementRef;
   @ViewChild('github') public github: ElementRef;
   @ViewChild('stackoverflow') public stackoverflow: ElementRef;
+
 
 
   public routes = [
@@ -34,15 +35,36 @@ export class AboutMeComponent implements OnInit {
     }
   ];
 
-  constructor(private router: Router) {
+  @HostListener('window:scroll', ['$event']) onScrollEvent($event) {
+    console.log('Page ' + window.pageYOffset);
+    console.log('Section ' + this.bio.nativeElement.offsetTop);
+    // console.debug("Scroll Event", window.pageYOffset );
+    if (window.pageYOffset < this.github.nativeElement.offsetTop - 400) {
+      this.activeButton('bio');
+    } else if (window.pageYOffset < this.stackoverflow.nativeElement.offsetTop - 400) {
+      this.activeButton('github');
+    } else {
+      this.activeButton('stackoverflow');
+    }
+  }
 
+  constructor() { }
+
+  activeButton(route: string) {
+    this.routes.forEach(element => {
+      if (element.route === route) {
+        element.isActive = true;
+      } else {
+        element.isActive = false;
+      }
+    });
   }
 
   internalRoute(segmantName: string) {
     this.routes.forEach(element => {
       element.isActive = false;
     });
-    this.routes.find(x => x.route === segmantName).isActive = true;
+    this.activeButton(segmantName);
 
     switch (segmantName) {
       case 'bio':
