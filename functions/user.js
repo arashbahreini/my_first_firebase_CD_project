d345ed615ba7491cad944a652fb4142f6a198825
arash bahreini <vbhost.ir@gmail.com>
2019-04-16 17:18:59 +0430

    fix eslint issues for deploy

--- functions/user.js
+++ functions/user.js
@@ -35,32 +35,37 @@ const claims = {
 // })
 
 userApp.post('/user/login', (req, res) => {
-  firebase.auth().signInWithEmailAndPassword(req.body.email, req.body.password).then(loginRes => {
-    res.send(loginRes);
-  }).catch(error => {
-    res.send(error);
-  })
+  firebase.auth().signInWithEmailAndPassword(req.body.email, req.body.password)
+    .then(loginRes => {
+      res.send(loginRes);
+      return loginRes;
+    }).catch(error => {
+      res.send(error);
+      return error;
+    })
 })
 
 userApp.get('/user/getUsers', (req, res) => {
   admin.auth().listUsers().then((listResult) => {
     res.send(listResult.users);
-  }, (error) => {
+    return listResult.users;
+  }).catch(error => {
     res.send(error);
-  })
+    return error;
+  });
 })
+
+
 
 userApp.post('/user/getToken', (req, res) => {
   admin.auth().createCustomToken(req.body.uid, claims)
-  .then((customToken) => {
-    res.send(customToken);
-  }).catch((error) => {
-    res.send(error);
-  })
-})
-
-userApp.get('/user/test', (req, res) => {
-  res.send({ 'Name': 'Arash' });
+    .then((customToken) => {
+      res.send(customToken);
+      return customToken;
+    }).catch((error) => {
+      res.send(error);
+      return error;
+    })
 })
 
 exports.userApp = functions.https.onRequest(userApp);
