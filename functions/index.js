47b7cbcc19bac3c1031164ca5fd65d5795f9f2fc
arash bahreini <vbhost.ir@gmail.com>
2019-04-16 11:54:26 +0430

    Design user APIs in backend.

--- functions/index.js
+++ functions/index.js
@@ -1,83 +1,12 @@
-const firebase = require('firebase');
-
-var config = {
-  apiKey: 'AIzaSyDlib_eSWxnRwHnABW8YwxyUo-uHD3EMPg',
-  authDomain: 'me-arash.firebaseapp.com',
-  databaseURL: 'https://me-arash.firebaseio.com',
-  projectId: 'me-arash',
-  storageBucket: 'me-arash.appspot.com',
-  messagingSenderId: '305977168091'
-};
-
-firebase.initializeApp(config);
-// firebase.database().ref('test/').push({
-//   name: 'arash',
-//   family: 'bahreini'
-// })
-
-// firebase.database().ref('test').remove();
-
-// firebase.database().ref('users').push({
-//   id: 1,
-//   email: 'vbhost',
-//   passwordHash: Buffer.from('arash'),
-//   passwordSalt: Buffer.from('salt'),
-// });
-
-// firebase.database().ref('users').once('value').then((data) => {
-//   console.log(data);
-// });
-
-
-
-let userImportRecords = [
-  {
-    uid: 'uid1',
-    email: 'user1@example.com',
-    passwordHash: Buffer.from('passwordHash1'),
-    passwordSalt: Buffer.from('salt1')
-  },
-  {
-    uid: 'uid2',
-    email: 'user2@example.com',
-    passwordHash: Buffer.from('passwordHash2'),
-    passwordSalt: Buffer.from('salt2')
-  },
-];
-const admin = require('firebase-admin');
-
 const functions = require('firebase-functions');
 const express = require('express');
-const serviceAccount = require('./me-arash-firebase-adminsdk.json');
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
-
-const uid = 'ACA0EDA3-9ED5-42EE-A849-11B14D376F87';
-const claims = {
-  premiumAccount: true
-};
-
-
 const app = express();
-app.get('/login', (req, res) => {
-  admin.auth().createCustomToken(uid, claims)
-    .then((customToken) => {
-      res.send(customToken);
-    }).catch((error) => {
-      res.send('Its error' + error);
-    })
-})
-
-app.post('/user/login', (req, res) => {
-  firebase.auth().signInWithEmailAndPassword(req.body.email, req.body.password).then(loginRes => {
-    res.send(loginRes.user.uid);
-  }).catch(error => {
-    res.send(error);
-  })
+const user = require('./user');
+app.get('/test', (req, res) => {
+  res.send({ 'Name': 'Arash' });
 })
 
 exports.app = functions.https.onRequest(app);
-
+exports.userApp = functions.https.onRequest(user.userApp);
 
 
--- functions/user.js
+++ functions/user.js
@@ -1,47 +1,50 @@
-
 const config = require('./config');
 const admin = require('firebase-admin');
 const firebase = require('firebase').initializeApp(config.hostConfig);
 const serviceAccount = require('./me-arash-firebase-adminsdk.json');
+const functions = require('firebase-functions');
 const express = require('express');
+const userApp = express();
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
-const app = express();
-
-app.get('/user/create', (req, res) => {
-  admin.auth().createUser({
-    email: req.email,
-    disabled: req.disabled,
-    displayName: req.displayName,
-    emailVerified: req.emailVerified,
-    password: req.password,
-  }).then(createUserRes => {
-    res.send(createUserRes);
-  });
-})
-
-app.post('/user/login', (req, res) => {
-  firebase.auth().signInWithEmailAndPassword(req.email, req.password).then(loginRes => {
-    res.send(loginRes.user.uid);
-  }).catch(error => {
-    res.send(error);
-  })
-})
-
 
 const claims = {
   premiumAccount: true
 };
 
+// userApp.get('/user/create', (req, res) => {
+//   admin.auth().createUser({
+//     email: req.email,
+//     disabled: req.disabled,
+//     displayName: req.displayName,
+//     emailVerified: req.emailVerified,
+//     password: req.password,
+//   }).then(createUserRes => {
+//     res.send(createUserRes);
+//   });
+// })
 
-app.get('/user/createCustomToken', (req, res) => {
-  admin.auth().createCustomToken(req.uid, claims)
-    .then((customToken) => {
-      res.send(customToken);
-    }).catch((error) => {
-      res.send(error);
-    })
+userApp.post('/user/login', (req, res) => {
+  firebase.auth().signInWithEmailAndPassword(req.body.email, req.body.password).then(loginRes => {
+    res.send(loginRes);
+  }).catch(error => {
+    res.send(error);
+  })
 })
 
+userApp.post('/user/getToken', (req, res) => {
+  admin.auth().createCustomToken(req.body.uid, claims)
+  .then((customToken) => {
+    res.send(customToken);
+  }).catch((error) => {
+    res.send(error);
+  })
+})
+
+userApp.get('/user/test', (req, res) => {
+  res.send({ 'Name': 'Arash' });
+})
+
+exports.userApp = functions.https.onRequest(userApp);
