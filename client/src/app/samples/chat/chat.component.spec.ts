1bcf84110ae3ad2da2c558b71fbf195751e815a3
arash bahreini <vbhost.ir@gmail.com>
2019-05-01 14:49:25 +0430

    Wrote some tests on chat component.

--- client/src/app/samples/chat/chat.component.spec.ts
+++ client/src/app/samples/chat/chat.component.spec.ts
@@ -1,25 +1,37 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ChatComponent } from './chat.component';
+import { MaterialModule } from 'src/app/shared/material/material.module';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ChatUserDialogComponent } from './user/chat-user-dialog.component';
+import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
+import { ChatModule } from './chat.module';
 
 describe('ChatComponent', () => {
   let component: ChatComponent;
   let fixture: ComponentFixture<ChatComponent>;
-
+  let h5: HTMLElement;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ChatComponent ]
-    })
-    .compileComponents();
+      declarations: [],
+      imports: [
+        MaterialModule,
+        FormsModule,
+        BrowserAnimationsModule,
+        ChatModule,
+      ],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ChatComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    h5 = fixture.nativeElement.querySelector('h5');
   });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('Should show description', () => {
+    expect(h5.textContent).toEqual('Description about this page');
+  });
 });
