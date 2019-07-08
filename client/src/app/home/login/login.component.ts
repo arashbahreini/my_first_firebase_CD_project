fd19ce9c4d7fa06fcc8ab187915659b663853d15
arash bahreini <vbhost.ir@gmail.com>
2019-07-08 11:26:04 +0430

    Fix issue in menus to being visible in mobile.

--- client/src/app/home/login/login.component.ts
+++ client/src/app/home/login/login.component.ts
@@ -3,6 +3,7 @@ import { AuthenticationService } from 'src/app/services/authentication.service';
 import { Router } from '@angular/router';
 import { UserModel } from 'src/app/model/user.model';
 import { ResultModel } from 'src/app/model/result.model';
+import { CommonService } from 'src/app/services/common.service';
 
 @Component({
   selector: 'app-login',
@@ -10,13 +11,16 @@ import { ResultModel } from 'src/app/model/result.model';
   styleUrls: ['./login.component.sass']
 })
 export class LoginComponent implements OnInit {
-
+  public isWithMobile: boolean;
   public user: UserModel = new UserModel();
   public loginResult: ResultModel<any> = new ResultModel<any>();
 
   constructor(
     private authenticationService: AuthenticationService,
-    private routes: Router) { }
+    private routes: Router,
+    private commonService: CommonService) {
+    this.isWithMobile = commonService.getUserPlatform().isWithMobile;
+  }
 
   ngOnInit() {
   }
